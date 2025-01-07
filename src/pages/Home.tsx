60642b37e13fc083d56c288031c15b7f565cfd96
Yayan57 <adriangarza115@yahoo.com>
2025-01-06 23:48:50 -0600

    css updates, featured game card(in progress), LEC schedule data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,6 +32,9 @@ import {
   useIonAlert,
   useIonToast,
   useIonViewWillEnter,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
 import {
   addOutline,
@@ -43,8 +46,36 @@ import {
 import React, { useEffect, useRef, useState } from "react";
 import "./Home.css";
 import "./Search";
+import logo1 from "../assets/images/FaZe-Clan-OnDark.png";
+import logo2 from "../assets/images/Sangal-eSports.png";
+import video1 from "../assets/images/cs2_thumbnail.webp";
+import video2 from "../assets/images/lol_thumbnail.jfif";
+import video3 from "../assets/images/rl_thumbnail.jfif";
 
 const Home: React.FC = () => {
+  const swiperHeadlines = [
+    "Faker hits 3k",
+    "DRX steals baron",
+    "Teddy escapes certain death",
+    "Doran saves Keria",
+  ];
+
+  const swiperVideos = [video1, video2, video3, video1];
+
+  const featuredGame = [
+    {
+      team1: "T1",
+      team2: "DRX",
+      score1: 3,
+      score2: 2,
+      team1logo: logo1,
+      team2logo: logo2,
+      team1color: "red",
+      team2color: "blue",
+      swiperHeadlines: swiperHeadlines,
+      swiperVideos: swiperVideos,
+    },
+  ];
   const articles = [
     {
       headline: "Faker Leads T1 to Another Championship Victory at Worlds 2025",
@@ -116,13 +147,63 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        {featuredGame.map((game) => {
+          return (
+            <IonCard className="featuredGame-card">
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <IonTitle
+                  style={{
+                    background: `linear-gradient(to right, ${game.team1color}, ${game.team2color})`,
+                    color: "white",
+                    flex: 1,
+                    textAlign: "center",
+                  }}
+                >
+                  <IonGrid>
+                    <IonRow>
+                      <IonCol size="3" style={{ textAlign: "center" }}>
+                        {game.team1}
+                      </IonCol>
+                      <IonCol size="6" />
+                      <IonCol size="3" style={{ textAlign: "center" }}>
+                        {game.team2}
+                      </IonCol>
+                    </IonRow>
+                  </IonGrid>
+                </IonTitle>
+              </div>
+              <IonImg src={game.team1logo} className="score-image" />
+              <IonText>
+                {game.team1}
+                {game.team2}
+              </IonText>
+              <IonImg src={game.team2logo} className="score-image" />
+              <IonList className="swiper-container">
+                {game.swiperHeadlines.map((headline, index) => (
+                  <IonCard key={index} className="swiper-card">
+                    <IonImg src={game.swiperVideos[index]} />
+                    <IonCardHeader>
+                      <IonCardTitle>{headline}</IonCardTitle>
+                    </IonCardHeader>
+                  </IonCard>
+                ))}
+              </IonList>
+              <IonButton>GameCast</IonButton>
+              <IonButton>BoxScore</IonButton>
+            </IonCard>
+          );
+        })}
         <IonCard className="headline-card">
           <IonTitle className="headline-title">Top Headlines</IonTitle>
           <div className="headline-divider" />
           {articles.map((article) => {
             return (
               <div className="headlines">
-                <IonIcon slot="icon-only" icon={listOutline} color="dark" />
+                <IonIcon
+                  slot="icon-only"
+                  icon={listOutline}
+                  className="list-icon"
+                />
                 <a
                   href={article.link}
                   target="_blank"
